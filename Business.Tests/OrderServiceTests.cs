1943f51ae736f45b91d2babd54b06dc37f969536
Okan Celik <okan.celik@outlook.com>
2021-07-30 09:16:43 +0300

    Test edited.

--- Business.Tests/OrderServiceTests.cs
+++ Business.Tests/OrderServiceTests.cs
@@ -1,12 +1,10 @@
 ﻿using AutoMapper;
-using Business.Abstruct;
 using Business.Concreate;
 using Business.Mapping;
 using Data.MongoDb.Abstruct;
 using Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -31,30 +29,30 @@ namespace Business.Tests
             {
                 new Order
                 {
-                    Id = new Guid().ToString(),
+                    Id = "1",
                     Name = "Test 1",
                     Description = "Description 1",
                     CategoryId = "1"
                 },
                 new Order
                 {
-                    Id = new Guid().ToString(),
-                    Name = "Test 1",
-                    Description = "Description 1",
+                    Id = "2",
+                    Name = "Test 2",
+                    Description = "Description 2",
                     CategoryId = "1"
                 },
                 new Order
                 {
-                    Id = new Guid().ToString(),
-                    Name = "Test 1",
-                    Description = "Description 1",
+                    Id = "3",
+                    Name = "Test 3",
+                    Description = "Description 3",
                     CategoryId = "1"
                 },
                 new Order
                 {
-                    Id = new Guid().ToString(),
-                    Name = "Test 1",
-                    Description = "Description 1",
+                    Id = "4",
+                    Name = "Test 4",
+                    Description = "Description 4",
                     CategoryId = "1"
                 }
             };
@@ -83,8 +81,9 @@ namespace Business.Tests
             _mapper = mockMapper.CreateMapper();
 
             _categoryRepository.Setup(x => x.GetAllAsync()).Returns(Task.Run(()=> _categories));
-            _categoryRepository.Setup(x => x.GetByIdAsync(It.IsAny<string>())).Returns(Task.Run(() => _categories.FirstOrDefault(x=> x.Id == It.IsAny<string>())));
+            _categoryRepository.Setup(x => x.GetByIdAsync(It.IsAny<string>())).Returns<string>((id) => Task.Run(() => _categories.FirstOrDefault(x=> x.Id == id)));
             _orderRepository.Setup(x => x.GetAllAsync()).Returns(Task.Run(() => _orders));
+            _orderRepository.Setup(x => x.GetByIdAsync(It.IsAny<string>())).Returns<string>((id) => Task.Run(() => _orders.FirstOrDefault(x => x.Id == id)));
         }
 
         [TestMethod]
@@ -93,6 +92,14 @@ namespace Business.Tests
             OrderService service = new OrderService(_categoryRepository.Object, _orderRepository.Object, _mapper);
             var result = service.GetAllAsync();
             Assert.AreEqual(4, result.Result?.Count);
+        }
+
+        [TestMethod]
+        public void GetByIdAsync()
+        {
+            OrderService service = new OrderService(_categoryRepository.Object, _orderRepository.Object, _mapper);
+            var result = service.GetByIdAsync("1");
+            Assert.IsNotNull(result.Result);
         }
     }
 }
